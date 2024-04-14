63d37532efa0de392ed3d6e4c23f8463b0e8a317
zxcv9675 <zxcv9675@naver.com>
2024-04-14 16:54:40 +0900

    백준 알고리즘 연습

---
+++ 1단계/Main.java
@@ -0,0 +1,218 @@
+/*  2557번
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+    }
+}
+ */
+
+/* 1000번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+    }
+}
+*/
+
+/* 1001번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A-B);
+    }
+}
+
+ */
+
+/* 10998 번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A*B);
+    }
+}
+
+ */
+
+/* 1008번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc= new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println((double)A/B);
+    }
+}
+
+
+ */
+
+/* 10869번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
+
+ */
+
+/* 10926번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        System.out.println(name+"??!");
+    }
+}
+
+ */
+
+/* 18108번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        System.out.println(year-543);
+    }
+}
+
+
+ */
+
+/* 10430번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
+
+ */
+
+/* 2588번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A*(B%10));
+        System.out.println(A*((B%100)/10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
+    }
+}
+
+ */
+
+/* 11382번
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A+B+C);
+    }
+}
+
+ */
+
+/* 10171번
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
+
+ */
+
+/* 10172번
+public class Main{
+    public static void main(String[] args){
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+ */
+
+
+
+
+
