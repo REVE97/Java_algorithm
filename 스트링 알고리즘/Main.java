690c1ca5c8b0069c3009ddcd70251300c3502a4d
zxcv9675 <zxcv9675@naver.com>
2024-04-15 21:57:16 +0900

    백준 알고리즘 연습

--- 스트링 알고리즘/Main.java
+++ 스트링 알고리즘/Main.java
@@ -27,3 +27,46 @@ public class Main {
 
  */
 
+/* 1543번
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static String text;
+    static String pattern;
+    static int cnt = 0;
+    static int i = 0;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        text = br.readLine();
+        pattern = br.readLine();
+        int t = text.length();
+        int p = pattern.length();
+
+        while (i < t-p+1) {
+            int startIdx = i;
+            boolean flag = true;
+            for (int j = 0; j < p; j++) {
+                if (pattern.charAt(j) != text.charAt(startIdx)) {
+                    i += 1;
+                    flag = false;
+                    break;
+                } else {
+                    startIdx += 1;
+                }
+            }
+
+            if (flag) {
+                cnt += 1;
+                i += p;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
+
+ */
+
