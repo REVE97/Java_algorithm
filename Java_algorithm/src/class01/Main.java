9d845ca7bfdf10a58f1ff2ccc09b4d0c8e34e6c0
zxcv9675 <zxcv9675@naver.com>
2024-11-27 17:06:14 +0900

    class01 code update

--- Java_algorithm/src/class01/Main.java
+++ Java_algorithm/src/class01/Main.java
@@ -1,12 +1,13 @@
 //solved.ac_class01
 
-//p.1000
-package class01;
+package class01; //패키지 선언
+
+/*p.1000
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
         int A= sc.nextInt();
@@ -15,3 +16,215 @@ public class Main {
         System.out.println(A+B);
     }
 }
+*/
+
+/*p.1001
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.print(A-B);
+    }
+}
+*/
+
+/*p.1008
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A/B);
+    }
+}
+*/
+
+/*p.2438
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*p.1330
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if(A>B){
+            System.out.print('>');
+        } else if(A<B){
+            System.out.print('<');
+        } else {
+            System.out.print("==");
+        }
+    }
+}
+*/
+
+/*p.2475
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int sum = a*a + b*b + c*c + d*d + e*e;
+        int result = sum % 10;
+
+        System.out.print(result);
+    }
+}
+*/
+
+/*p.2557
+public class Main{
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
+*/
+
+/*p.2739
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<10;i++){
+            System.out.println(N + " * " + i + " = " + N*i);
+        }
+    }
+}
+*/
+
+/*p.2741
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=N;i++){
+            System.out.println(i);
+        }
+    }
+}*/
+
+/*p.2753
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int year = Integer.parseInt(br.readLine());
+
+        if(((year%4==0)&&(year%100!=0))||(year%400==0)){
+            System.out.print(1);
+        } else {
+            System.out.print(0);
+        }
+    }
+}*/
+
+/*p.9498
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        if(score>=90){
+            System.out.print('A');
+        } else if(score>=80){
+            System.out.print('B');
+        } else if(score>=70){
+            System.out.print('C');
+        } else if(score>=60){
+            System.out.print('D');
+        } else {
+            System.out.print('F');
+        }
+    }
+}*/
+
+/*p.10171
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}*/
