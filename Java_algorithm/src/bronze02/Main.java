910ec403095eb9fa99dbe80b877b17216f66ed25
zxcv9675 <zxcv9675@naver.com>
2024-12-11 16:16:23 +0900

    Baekjoon bronze02 code

---
+++ Java_algorithm/src/bronze02/Main.java
@@ -0,0 +1,33 @@
+/*FeedBack
+
+*/
+package bronze02; //패키지 선언
+
+/*p.2750
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}*/
+
+//p.2908
+
