f494ce81680ac0404c8ac0fe030c0a91591529ee
zxcv9675 <zxcv9675@naver.com>
2025-04-23 10:19:48 +0900

    Baekjoon class02 code update

--- Java_algorithm/src/class02/Main.java
+++ Java_algorithm/src/class02/Main.java
@@ -820,7 +820,7 @@ public class Main {
 // p.10828
 // 스택
 
-import java.io.BufferedReader;
+/*import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -828,6 +828,7 @@ import java.util.StringTokenizer;
 public class Main {
     static int[] stack = new int[10001];
     static int size;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -888,10 +889,89 @@ public class Main {
             return T;
         }
     }
+}*/
 
+// p.10845
+// 큐
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] queue = new int[10001];
+    static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+
+            switch (s) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+                case "front":
+                    sb.append(front()).append('\n');
+                    break;
+                case "back":
+                    sb.append(back()).append('\n');
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+
+    public static void push(int num){
+        queue[size] = num;
+        size++;
+    }
+
+    public static int pop(){
+        if(size == 0) return -1;
+        else {
+            int temp = queue[0];
+            for(int i = 1; i < size; i++){
+                queue[i-1] = queue[i];
+            }
+            size--;
+            return temp;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        return size == 0 ? 1 : 0;
+    }
+
+    public static int front() {
+        if(size == 0) return -1;
+        else return queue[0];
+    }
+
+    public static int back() {
+        if(size == 0) return -1;
+        else return queue[size - 1];
+    }
 }
-
-
 
 
 
