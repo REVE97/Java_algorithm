cfe94ae09c337cb4e218447841ad51f05b9e2f53
zxcv9675 <zxcv9675@naver.com>
2025-06-11 09:34:06 +0900

    Baekjoon code update

--- Java_algorithm/src/silver04/Main.java
+++ Java_algorithm/src/silver04/Main.java
@@ -108,6 +108,7 @@ public class Main {
 // p.18258
 // 큐
 
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -157,5 +158,40 @@ public class Main {
             }
         }
         System.out.println(sb);
+    }
+}*/
+
+// p.14425
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int count = 0;
+
+        Set<String> set1 = new HashSet<>();
+
+        for (int i = 0; i < N; i++) {
+            set1.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (set1.contains(br.readLine())) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
