17f83aebb768a94af19601e3c231b0d454080fe2
zxcv9675 <zxcv9675@naver.com>
2025-06-13 09:40:31 +0900

    Baekjoon code update

--- Java_algorithm/src/silver04/Main.java
+++ Java_algorithm/src/silver04/Main.java
@@ -249,6 +249,7 @@ public class Main {
 // p.1302
 // 집합, 맵
 
+/*
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -292,4 +293,83 @@ public class Main {
         Collections.sort(list2);
         System.out.println(list2.get(0));
     }
+}*/
+
+// p.28279
+// Deque
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 테스트케이스 수
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+
+            switch (num) {
+                case 1:
+                    dq.addFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case 2:
+                    dq.addLast(Integer.parseInt(st.nextToken()));
+                    break;
+                case 3:
+                    if(!dq.isEmpty()){
+                        sb.append(dq.removeFirst()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 4:
+                    if(!dq.isEmpty()){
+                        sb.append(dq.removeLast()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 5:
+                    sb.append(dq.size()).append("\n");
+                    break;
+                case 6:
+                    if(!dq.isEmpty()){
+                        sb.append(0).append("\n");
+                    } else {
+                        sb.append(1).append("\n");
+                    }
+                    break;
+                case 7:
+                    if(!dq.isEmpty()){
+                        sb.append(dq.peekFirst()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case 8:
+                    if(!dq.isEmpty()){
+                        sb.append(dq.peekLast()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
+
+
+
+
+
