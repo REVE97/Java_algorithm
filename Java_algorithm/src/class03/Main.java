e770f7229f51f908d7495b4d079ad05160503f24
zxcv9675 <zxcv9675@naver.com>
2025-06-16 11:45:29 +0900

    Baekjoon code update

--- Java_algorithm/src/class03/Main.java
+++ Java_algorithm/src/class03/Main.java
@@ -949,7 +949,7 @@ public class Main {
 // p.1012
 // DFS
 
-import java.io.BufferedReader;
+/*import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -1019,4 +1019,30 @@ public class Main {
             }
         }
     }
+}*/
+
+// p.17626
+// DP
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // 최악의 경우: 1^2 + 1^2 + ... + 1^2
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
 }
+
